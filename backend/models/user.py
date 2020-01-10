923e48d8c03e4150ebd7a1ba482ca0e8db4874ab
jackson <jackwgoulart@gmail.com>
2020-01-10 20:32:17 +0000

    fix format file

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,16 +1,14 @@
 from backend import db
 
-class User(db.Model):
 
+class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     password = db.Column(db.String)
-    email = db.Column(db.String,)
-
+    email = db.Column(db.String, )
 
 
 class UserVocabulary(db.Model):
-
-    __tablename__='uservocabualary'
+    __tablename__ = 'uservocabualary'
